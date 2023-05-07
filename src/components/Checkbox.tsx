e3f48979aab47501e9cf656e14aa1525476347a5
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-05-07 06:34:22 +0100

    fix not showing completed todos as complete oninitial load

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,7 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-export default function Checkbox() {
+export default function Checkbox({ checked }: { checked: boolean }) {
   const [complete, setComplete] = useState(false);
+
+  useEffect(() => {
+    checked ? setComplete(true) : setComplete(false);
+  }, [checked]);
 
   return <div className={`checkbox ${complete && 'checked'}`} onClick={() => setComplete(!complete)}></div>;
 }

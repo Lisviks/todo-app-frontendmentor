d2cdd273ff8a4177fa9257d1769050e98a40ec30
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-05-08 06:16:11 +0100

    add new todo when pressing Enter key

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,9 +1,16 @@
 import { useTodosDispatch } from '@/context/TodosContext';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export default function AddTodo() {
   const [text, setText] = useState('');
   const dispatch = useTodosDispatch();
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      setText('');
+      dispatch({ type: 'ADD', id: Date.now(), text });
+    }
+  };
 
   return (
     <div className='input-field'>
@@ -14,11 +21,17 @@ export default function AddTodo() {
           dispatch({
             type: 'ADD',
             id: Date.now(),
-            text: text,
+            text,
           });
         }}
       ></div>
-      <input type='text' placeholder='Create a new todo...' value={text} onChange={(e) => setText(e.target.value)} />
+      <input
+        type='text'
+        placeholder='Create a new todo...'
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
     </div>
   );
 }

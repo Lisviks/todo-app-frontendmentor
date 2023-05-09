40c04ac0a33c2d3666a1c8214585d9dfcfce6b54
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-05-09 06:09:12 +0100

    display how many todos left on the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,12 @@
+import { useTodos } from '@/context/TodosContext';
+
 export default function Footer() {
+  const todos = useTodos();
+  const todosLeft = todos.filter((todo) => !todo.complete).length;
+
   return (
     <div className='footer'>
-      <div className='items-left'>2 items left</div>
+      <div className='items-left'>{todosLeft} items left</div>
       <ul className='view-list'>
         <li className='current'>All</li>
         <li>Active</li>

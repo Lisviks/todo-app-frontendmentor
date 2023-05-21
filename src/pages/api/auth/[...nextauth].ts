1ae373019c60f67e3d39e2eae67f985aed0dca0f
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-05-21 18:53:55 +0100

    switch to mongodb adapter

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import MongooseAdapter from '@/lib/auth-adapter/mongoose-adapter';
 import dbConnect from '@/lib/dbConnect';
+import clientPromise from '@/lib/mongodb';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
@@ -24,7 +26,8 @@ export default NextAuth({
     }),
   ],
   secret: getEnvVar('NEXTAUTH_SECRET'),
-  adapter: MongooseAdapter(dbConnect),
+  // adapter: MongooseAdapter(dbConnect),
+  adapter: MongoDBAdapter(clientPromise),
   callbacks: {
     session: async ({ session, user }) => {
       session.user.id = user.id.toString();

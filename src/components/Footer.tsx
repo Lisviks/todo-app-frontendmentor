949b4636aa21ff07eb8c378451176f43ff706894
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-05-22 07:42:59 +0100

    update todo ids array when deleting completed todos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,29 @@
 import { useTodos, useTodosDispatch } from '@/context/TodosContext';
-import { deleteCompleteTodos } from '@/context/actions';
+import { deleteCompleteTodos, saveTodoIds } from '@/context/actions';
+import { useSession } from 'next-auth/react';
 
 export default function Footer() {
-  const { todos, filter } = useTodos();
+  const {
+    todos,
+    filter,
+    todoIds: { ids },
+  } = useTodos();
   const dispatch = useTodosDispatch();
+  const { data: session } = useSession();
   const todosLeft = todos.filter((todo) => !todo.complete).length;
 
   const changeFilter = (newFilter: string) => {
     dispatch({ type: 'FILTER_TODO', filter: newFilter });
+  };
+
+  const handleDeleteComplete = () => {
+    const filteredTodoIds = ids.filter((id, index) => {
+      if (!todos[index].complete) {
+        return id;
+      }
+    });
+    deleteCompleteTodos(dispatch);
+    saveTodoIds(session?.user.id as string, filteredTodoIds, dispatch);
   };
 
   return (
@@ -24,7 +40,7 @@ export default function Footer() {
           Completed
         </li>
       </ul>
-      <div className='clear-completed' onClick={() => deleteCompleteTodos(dispatch)}>
+      <div className='clear-completed' onClick={handleDeleteComplete}>
         Clear Completed
       </div>
     </div>
